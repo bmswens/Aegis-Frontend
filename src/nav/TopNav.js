5997d33cab93c91c388fcc035447975d8ea63646
Brandon Swenson <bmswens@gmail.com>
2023-02-23 01:59:46 -0700

    Added profile button to <TopNav>

--- src/nav/TopNav.js
+++ src/nav/TopNav.js
@@ -6,8 +6,15 @@ import { AppBar, Box, IconButton, Toolbar, Tooltip } from '@mui/material'
 
 // MUI Icons
 import ShieldIcon from '@mui/icons-material/Shield'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import UserDialog from '../dialog/UserDialog'
 
 function TopNav(props) {
+
+    const [profileOpen, setProfileOpen] = React.useState(false)
+    function close() {
+        setProfileOpen(false)
+    }
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -23,8 +30,22 @@ function TopNav(props) {
                         </IconButton>
                     </Tooltip>
                     <Box sx={{ flexGrow: 1 }} />
+                    <Tooltip
+                        title="User Profile"
+                    >
+                        <IconButton
+                            onClick={() => setProfileOpen(true)}
+                            aria-label="User Profile"
+                        >
+                            <AccountCircleIcon fontSize="large" />
+                        </IconButton>
+                    </Tooltip>
                 </Toolbar>
             </AppBar>
+            <UserDialog
+                open={profileOpen}
+                close={close}
+            />
         </Box>
     )
 }
--- src/nav/TopNav.test.js
+++ src/nav/TopNav.test.js
@@ -1,10 +1,31 @@
 // testing help
-import { render } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { act } from 'react-dom/test-utils'
+import { UserContextProvider } from '../context/UserContext'
 
 // to test
 import TopNav from './TopNav'
 
 describe('<TopNav>', function() {
+    it('should open the user dialog on click and be able to close', async function() {
+        const user = userEvent.setup()
+        render(
+            <UserContextProvider>
+                <TopNav />
+            </UserContextProvider>
+        )
+        let profileButton = screen.getByRole("button", { name: "User Profile"})
+        await act(() => user.click(profileButton))
+        let profileDialog = screen.getByRole("dialog", { name: "User Settings"})
+        expect(profileDialog).not.toBeNull()
+        let closeButton = screen.getByRole("button", { name: "Close" })
+        await act(() => user.click(closeButton))
+        await waitFor(() => {
+            let dialog = screen.queryByRole("dialog")
+            expect(dialog).toBeNull()
+        })
+    })
     it("doesn't do much... yet", function() {
         render(
             <TopNav />
