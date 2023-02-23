42011c4e808ae7c848988b5af9963bbbd440f55f
Brandon Swenson <bmswens@gmail.com>
2023-02-22 23:45:21 -0700

    Unneeded files removed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+// to test
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App>', function() {
+  it('should render without error', function() {
+    // all child components should be tested
+    render(<App />)
+  })
+})
