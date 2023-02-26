4e0b5f05008ec250c0ba139910938c8550e1aad7
Brandon Swenson <bmswens@gmail.com>
2023-02-25 17:47:03 -0700

    search added to <People>  page

--- src/content/people/People.js
+++ src/content/people/People.js
@@ -2,12 +2,53 @@
 import React from 'react'
 
 // MUI
-import { Grid, Skeleton } from '@mui/material'
+import { Button, Card, CardContent, Grid, Skeleton, TextField } from '@mui/material'
 
 // custom
 import { ContentGrid } from '../Content'
 import PersonCard from './PersonCard'
 import api from '../../api'
+import { Stack } from '@mui/system'
+
+function PersonSearch(props) {
+    const { search, setSearch } = props
+    const [ text, setText ] = React.useState(search)
+    return (
+        <Grid item xs={12}>
+            <Card
+                sx={{
+                    width: "100%"
+                }}
+            >
+                <CardContent>
+                    <Stack
+                        alignItems="flex-end"
+                        spacing={1}
+                    >
+                        <TextField
+                            fullWidth
+                            type="search"
+                            label="Search"
+                            value={text}
+                            onChange={event => setText(event.target.value)}
+                            onKeyDown={event => {
+                                if (event.key === "Enter") {
+                                    setSearch(text)
+                                }
+                            }}
+                        />
+                        <Button
+                            variant="contained"
+                            onClick={() => setSearch(text)}
+                        >
+                            Search
+                        </Button>
+                    </Stack>
+                </CardContent>
+            </Card>
+        </Grid>
+    )
+}
 
 function PersonSkeleton(props) {
     return (
@@ -23,24 +64,70 @@ function PersonSkeleton(props) {
 
 function People(props)  {
 
+    // data load
     const [loading, setLoading] = React.useState(true)
     const [people, setPeople] = React.useState([])
+    const [toDisplay, setToDisplay] = React.useState([])
 
     React.useEffect(() => {
         async function load() {
             let p = await api.people.getPeople()
             setPeople(p)
+            setToDisplay(p)
             setLoading(false)
         }
         load()
     }, [])
 
+    // search
+    // TODO: May slow down if there are too many people.
+    // If that happens, switch to server-side searching.
+    const [ search, setSearch ] = React.useState('')
+
+    // sets the skeletons so users know something is happening
+    React.useEffect(() => {
+        setLoading(true)
+    }, [search])
+
+    // we're going to use case insensitive search
+    React.useEffect(() => {
+        if (people && search && loading) {
+            let lSearch = search.toLowerCase()
+            let output = []
+            for (let person of people) {
+                let fullName = `${person.firstName} ${person.lastName}`.toLowerCase()
+                if (fullName.includes(lSearch)) {
+                    output.push(person)
+                }
+                else if (person.email.toLowerCase().includes(lSearch)) {
+                    output.push(person)
+                }
+                else if (person.phone.toLowerCase().includes(lSearch)) {
+                    output.push(person)
+                }
+                else if (person.address.toLowerCase().includes(lSearch)) {
+                    output.push(person)
+                }
+            }
+            setToDisplay(output)
+            setLoading(false)
+        }
+        else if (people && loading && !search) {
+            setToDisplay(people)
+            setLoading(false)
+        }
+    }, [people, search, loading, setToDisplay])
+
     return (
         <ContentGrid>
+            <PersonSearch
+                search={search}
+                setSearch={setSearch}
+            />
             { loading ?
                 [1, 2, 3, 4, 5].map(n => <PersonSkeleton key={n} />)
                 :
-                people.map(person => <PersonCard key={person.email} {...person} />)
+                toDisplay.map(person => <PersonCard key={person.email} {...person} />)
             }
         </ContentGrid>
     )
--- src/content/people/People.test.js
+++ src/content/people/People.test.js
@@ -1,7 +1,10 @@
 // testing help
 import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { act } from 'react-dom/test-utils'
 import { BrowserRouter } from 'react-router-dom'
 import api from '../../api'
+import { demoUser } from '../../context/UserContext'
 
 // to test
 import People from './People'
@@ -20,6 +23,197 @@ describe('<People>', function() {
         await waitFor(() => {
             let buttons = screen.getAllByRole("button")
             expect(buttons.length).not.toBe(0)
+        })
+    })
+    it('should be able to query on name', async function() {
+        const user = userEvent.setup()
+        api.people.getPeople = jest.fn().mockResolvedValue([
+            {
+                ...demoUser,
+                firstName: "Brandon"
+            },
+            {
+                ...demoUser,
+                firstName: "Dude",
+                email: "dude@gmail.com"
+            }
+        ])
+        render(
+            <BrowserRouter>
+                <People />
+            </BrowserRouter>
+        )
+        await waitFor(() => {
+            expect(api.people.getPeople).toHaveBeenCalled()
+        })
+        let textField = screen.getByLabelText("Search")
+        await act(() => user.type(textField, "Brandon"))
+        let searchButton = screen.getByRole("button", { name: "Search"})
+        await act(() => user.click(searchButton))
+        await waitFor(() => {
+            let textButton = screen.getAllByRole("button", { name: "Send Text"})
+            expect(textButton).toHaveLength(1)
+        })
+    })
+    it('should be able to query on email', async function() {
+        const user = userEvent.setup()
+        api.people.getPeople = jest.fn().mockResolvedValue([
+            {
+                ...demoUser,
+                firstName: "Brandon"
+            },
+            {
+                ...demoUser,
+                firstName: "Dude",
+                email: "dude@gmail.com"
+            }
+        ])
+        render(
+            <BrowserRouter>
+                <People />
+            </BrowserRouter>
+        )
+        await waitFor(() => {
+            expect(api.people.getPeople).toHaveBeenCalled()
+        })
+        let textField = screen.getByLabelText("Search")
+        await act(() => user.type(textField, "dude@gmail.com"))
+        let searchButton = screen.getByRole("button", { name: "Search"})
+        await act(() => user.click(searchButton))
+        await waitFor(() => {
+            let textButton = screen.getAllByRole("button", { name: "Send Text"})
+            expect(textButton).toHaveLength(1)
+        })
+    })
+    it('should be able to query on phone number', async function() {
+        const user = userEvent.setup()
+        api.people.getPeople = jest.fn().mockResolvedValue([
+            {
+                ...demoUser,
+                firstName: "Brandon"
+            },
+            {
+                ...demoUser,
+                firstName: "Dude",
+                email: "dude@gmail.com",
+                phone: "911"
+            }
+        ])
+        render(
+            <BrowserRouter>
+                <People />
+            </BrowserRouter>
+        )
+        await waitFor(() => {
+            expect(api.people.getPeople).toHaveBeenCalled()
+        })
+        let textField = screen.getByLabelText("Search")
+        await act(() => user.type(textField, "911"))
+        let searchButton = screen.getByRole("button", { name: "Search"})
+        await act(() => user.click(searchButton))
+        await waitFor(() => {
+            let textButton = screen.getAllByRole("button", { name: "Send Text"})
+            expect(textButton).toHaveLength(1)
+        })
+    })
+    it('should be able to query on address', async function() {
+        const user = userEvent.setup()
+        api.people.getPeople = jest.fn().mockResolvedValue([
+            {
+                ...demoUser,
+                firstName: "Brandon"
+            },
+            {
+                ...demoUser,
+                firstName: "Dude",
+                email: "dude@gmail.com",
+                phone: "911",
+                address: "3rd St"
+            }
+        ])
+        render(
+            <BrowserRouter>
+                <People />
+            </BrowserRouter>
+        )
+        await waitFor(() => {
+            expect(api.people.getPeople).toHaveBeenCalled()
+        })
+        let textField = screen.getByLabelText("Search")
+        await act(() => user.type(textField, "3rd St"))
+        let searchButton = screen.getByRole("button", { name: "Search"})
+        await act(() => user.click(searchButton))
+        await waitFor(() => {
+            let textButton = screen.getAllByRole("button", { name: "Send Text"})
+            expect(textButton).toHaveLength(1)
+        })
+    })
+    it('should be able to query and then reset', async function() {
+        const user = userEvent.setup()
+        api.people.getPeople = jest.fn().mockResolvedValue([
+            {
+                ...demoUser,
+                firstName: "Brandon"
+            },
+            {
+                ...demoUser,
+                firstName: "Dude",
+                email: "dude@gmail.com",
+                phone: "911",
+                address: "3rd St"
+            }
+        ])
+        render(
+            <BrowserRouter>
+                <People />
+            </BrowserRouter>
+        )
+        await waitFor(() => {
+            expect(api.people.getPeople).toHaveBeenCalled()
+        })
+        let textField = screen.getByLabelText("Search")
+        await act(() => user.type(textField, "3rd St"))
+        let searchButton = screen.getByRole("button", { name: "Search"})
+        await act(() => user.click(searchButton))
+        await waitFor(() => {
+            let textButton = screen.getAllByRole("button", { name: "Send Text"})
+            expect(textButton).toHaveLength(1)
+        })
+        await act(() => user.clear(textField))
+        await act(() => user.click(searchButton))
+        await waitFor(() => {
+            let textButton = screen.getAllByRole("button", { name: "Send Text"})
+            expect(textButton).toHaveLength(2)
+        })
+    })
+    it('should be able to query on pressing {enter}', async function() {
+        const user = userEvent.setup()
+        api.people.getPeople = jest.fn().mockResolvedValue([
+            {
+                ...demoUser,
+                firstName: "Brandon"
+            },
+            {
+                ...demoUser,
+                firstName: "Dude",
+                email: "dude@gmail.com",
+                phone: "911",
+                address: "3rd St"
+            }
+        ])
+        render(
+            <BrowserRouter>
+                <People />
+            </BrowserRouter>
+        )
+        await waitFor(() => {
+            expect(api.people.getPeople).toHaveBeenCalled()
+        })
+        let textField = screen.getByLabelText("Search")
+        await act(() => user.type(textField, "3rd St{enter}"))
+        await waitFor(() => {
+            let textButton = screen.getAllByRole("button", { name: "Send Text"})
+            expect(textButton).toHaveLength(1)
         })
     })
 })
