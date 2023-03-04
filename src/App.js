ba48808deb106fb458633d9469835ee5fb145624
Brandon Swenson <bmswens@gmail.com>
2023-03-04 16:44:09 -0700

    update for github deployment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 // router
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 
 // custom
 import Content from "./content/Content"
 import Context from "./context/Context"
 import TopNav from "./nav/TopNav"
 
+function Router(props) {
+  const deployment = process.env.REACT_APP_DEPLOYMENT
+  /* istanbul ignore if: deployment config */
+  if (deployment === "github") {
+    return (
+      <HashRouter>
+        {props.children}
+      </HashRouter>
+    )
+  }
+  return (
+    <BrowserRouter>
+      {props.children}
+    </BrowserRouter>
+  )
+}
+
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.REACT_APP_BASENAME}>
+    <Router>
       <Context>
         <TopNav />
         <Content />
       </Context>
-    </BrowserRouter>
+    </Router>
   )
 }
 
